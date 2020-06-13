9be7d24932277e43d688a7e9b77b76bc17597ca3
Dibwe243 <dibwe.kalangu@umuzi.org>
2020-06-13 22:40:25 +0200

    Added bureau fake data

--- frontend/src/components/regions/bureau/index.js
+++ frontend/src/components/regions/bureau/index.js
@@ -2,8 +2,23 @@ import React from 'react';
 import Presentation from './Presentation';
 
 const Bureau = () => {
+    const bureauData = {
+        logo: 'logo url',
+        number: 'xxxxx',
+        type: 'type',
+        regNumber: 'organization\'s registration number',
+        industry: 'industry',
+        businessAddress: "",
+        website: 'url',
+        facebook: 'fb',
+        contact: { name: 'name', position: 'position', email: 'email', phone: 'Phone_1', phone: 'phone_2' },
+        operationalHour: '0h:00min to 23h:55min',
+        about: 'A very long text '
+    }
 
-    return < Presentation />
+
+    }
+return < Presentation data={bureauData}/>
 }
 
 export default Bureau;
