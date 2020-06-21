429b5834b953ccf5d8349405a591f78429c6f835
Dibwe243 <dibwe.kalangu@umuzi.org>
2020-06-21 21:20:54 +0200

    Added build files for live preview

---
+++ build/precache-manifest.0b7cd8b646e2253e6e9b94237037b18d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b4cc6f479e26c6e6bcb4414e82df7fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "030a3003270d57543e4c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "580a24506bc859897fe9",
+    "url": "/static/js/2.b2a7a271.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b2a7a271.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "030a3003270d57543e4c",
+    "url": "/static/js/main.15a9c25b.chunk.js"
+  },
+  {
+    "revision": "4b35d7dec53e4c348dee",
+    "url": "/static/js/runtime-main.90ee4741.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
