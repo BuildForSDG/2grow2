e053cad044c143b912f4ca576fc28209290580ee
Dibwe243 <dibwe.kalangu@umuzi.org>
2020-06-21 20:43:35 +0200

    removed backend directory

--- backend/__tests__/app.test.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest');
-const app = require('../src/app');
-
-describe('Test the root path', () => {
-  test('It should response the GET method', async () => {
-    const response = await request(app).get('/');
-    expect(response.statusCode).toBe(200);
-  });
-});
--- src/utils/config.js
+++
@@ -1,3 +0,0 @@
-const config = require('dotenv-safe').config().required;
-
-module.exports = config;
