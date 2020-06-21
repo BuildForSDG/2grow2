4c9d07c71eda0ded43dc0a30801afd628fa0fa98
Dibwe243 <dibwe.kalangu@umuzi.org>
2020-06-21 22:24:08 +0200

    commit build

---
+++ build/precache-manifest.ac919307e39a64911ba441835f53ab33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a09804b86bf0bb28431ab57d2e08090f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58dd243ecde6d0ff84fb",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f08a4f692f71ece91b01",
+    "url": "/static/js/2.f4306089.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f4306089.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58dd243ecde6d0ff84fb",
+    "url": "/static/js/main.deeee8ca.chunk.js"
+  },
+  {
+    "revision": "4b35d7dec53e4c348dee",
+    "url": "/static/js/runtime-main.90ee4741.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
